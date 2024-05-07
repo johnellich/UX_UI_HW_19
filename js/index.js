dbdae62fbd233c4499571e8da57bcc0dac0175c1
johnellich <153477535+johnellich@users.noreply.github.com>
2024-05-07 18:23:58 -0500

    fixed preloader

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-function fade() {
-    $('.preloader').fadeOut("slow");
-    }
-    setTimeout(fade, 3000);
+// function fade() {
+//     $('.preloader').fadeOut("slow");
+//     }
+//     setTimeout(fade, 3000);
+
+$(".preloader").delay(3000).fadeOut("slow",function(){
+    $(this).remove();
+})
